4a59406bdf7b306b10ed2446e071e072a91ed735
LouisBear123a <louisbear112@gmail.com>
2023-04-17 18:57:25 -0600

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,32 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
-from sklearn.metrics import accuracy_score
-
-class CommandDataset(Dataset):
-    def __init__(self, commands, tasks):
-        self.commands = commands
-        self.tasks = tasks
-    
-    def __len__(self):
-        return len(self.commands)
-    
-    def __getitem__(self, idx):
-        return self.commands[idx], self.tasks[idx]
-
-class CommandClassifier(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
-    
-    def forward(self, x):
-        embedded = self.embedding(x)
-        output, (hidden, cell) = self.lstm(embedded)
-        predictions = self.fc(hidden[-1])
-        return predictions.squeeze(1)
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import random_split
+from tqdm import tqdm
 
 class ModelTrainer:
     def __init__(self, train_data, val_data, vocab_size, embedding_dim, hidden_dim, output_dim, batch_size, lr, num_epochs):
@@ -39,75 +16,68 @@ class ModelTrainer:
         self.batch_size = batch_size
         self.lr = lr
         self.num_epochs = num_epochs
-    
-    def train_model(self):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        
-        # Define the model, optimizer, and loss function
-        model = CommandClassifier(self.vocab_size, self.embedding_dim, self.hidden_dim, self.output_dim).to(device)
-        optimizer = optim.Adam(model.parameters(), lr=self.lr)
-        criterion = nn.CrossEntropyLoss().to(device)
-        
-        # Create data loaders
-        train_dataset = CommandDataset(self.train_data['commands'], self.train_data['tasks'])
-        val_dataset = CommandDataset(self.val_data['commands'], self.val_data['tasks'])
-        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        val_loader = DataLoader(val_dataset, batch_size=self.batch_size)
-        
-        # Train the model
-        for epoch in range(self.num_epochs):
-            train_loss, train_acc = self.train_epoch(model, train_loader, optimizer, criterion, device)
-            val_loss, val_acc = self.evaluate_model(model, val_loader, criterion, device)
-            print(f'Epoch {epoch+1}/{self.num_epochs}: Train Loss: {train_loss:.3f}, Train Acc: {train_acc:.3f}, Val Loss: {val_loss:.3f}, Val Acc: {val_acc:.3f}')
-        
-        # Save the trained model
-        torch.save(model.state_dict(), 'command_classifier.pt')
-    
-    def train_epoch(self, model, data_loader, optimizer, criterion, device):
-        model.train()
-        total_loss = 0
-        total_correct = 0
-        for inputs, targets in data_loader:
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item() * inputs.size(0)
-            total_correct += self.get_num_correct(outputs, targets)
-        return total_loss / len(data_loader.dataset), total_correct / len(data_loader.dataset)
-    
-    def evaluate_model(self):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
-    
-    train_losses = []
-    val_losses = []
-    val_accs = []
-    for epoch in range(self.num_epochs):
-        train_loss = 0.0
-        self.model.train()
-        for inputs, targets in self.train_loader:
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            optimizer.zero_grad()
-            outputs = self.model(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-        train_loss /= len(self.train_loader)
-        train_losses.append(train_loss)
-        
-        val_loss, val_acc = self.evaluate_on_validation_set()
-        val_losses.append(val_loss)
-        val_accs.append(val_acc)
-        
-        print(f'Epoch {epoch + 1}/{self.num_epochs}:')
-        print(f'Train Loss: {train_loss:.3f} | Val Loss: {val_loss:.3f} | Val Acc: {val_acc:.3f}')
-    
-    self.save_model()
+        self.model = self.build_model()
+        self.train_loader, self.val_loader = self.build_loaders()
 
+    def build_model(self):
+        model = CommandClassifier(self.vocab_size, self.embedding_dim, self.hidden_dim, self.output_dim)
+        return model
+
+    def build_loaders(self):
+        train_loader = DataLoader(self.train_data, batch_size=self.batch_size, shuffle=True)
+        val_loader = DataLoader(self.val_data, batch_size=self.batch_size)
+        return train_loader, val_loader
+
+    def evaluate_on_validation_set(self):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        criterion = nn.CrossEntropyLoss()
+        with torch.no_grad():
+            val_loss = 0.0
+            val_acc = 0.0
+            self.model.eval()
+            for inputs, targets in self.val_loader:
+                inputs = inputs.to(device)
+                targets = targets.to(device)
+                outputs = self.model(inputs)
+                loss = criterion(outputs, targets)
+                val_loss += loss.item()
+                _, predicted = torch.max(outputs, 1)
+                val_acc += (predicted == targets).sum().item()
+            val_loss /= len(self.val_loader)
+            val_acc /= len(self.val_data)
+        return val_loss, val_acc
+
+    def evaluate_model(self):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        
+        train_losses = []
+        val_losses = []
+        val_accs = []
+        for epoch in range(self.num_epochs):
+            train_loss = 0.0
+            self.model.train()
+            for inputs, targets in self.train_loader:
+                inputs = inputs.to(device)
+                targets = targets.to(device)
+                optimizer.zero_grad()
+                outputs = self.model(inputs)
+                loss = criterion(outputs, targets)
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.item()
+            train_loss /= len(self.train_loader)
+            train_losses.append(train_loss)
+            
+            val_loss, val_acc = self.evaluate_on_validation_set()
+            val_losses.append(val_loss)
+            val_accs.append(val_acc)
+            
+            print(f'Epoch {epoch + 1}/{self.num_epochs}:')
+            print(f'Train Loss: {train_loss:.3f} | Val Loss: {val_loss:.3f} | Val Acc: {val_acc:.3f}')
+        
+        self.save_model()
+
+    def save_model(self):
+        torch.save(self.model.state_dict(), 'command_classifier.pt')
